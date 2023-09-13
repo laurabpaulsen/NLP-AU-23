88acc9227ae7de35b79f0dba47110e8e46d28c5a
laurabpaulsen <202005791@post.au.dk>
2023-09-13 10:46:44 +0200

    add requirements and setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# create env uisng venv
+python3 -m venv env
+
+# activate env
+source ./env/bin/activate
+
+python3 -m pip install --upgrade pip
+
+# install requirements
+python3 -m pip3 install -r requirements.txt
+
+# making sure we can use it in ipynb
+# python3 -m ipykernel install --user --name=env
